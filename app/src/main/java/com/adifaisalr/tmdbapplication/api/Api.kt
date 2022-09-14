5690c725ec217a56df0e397498695c1885f6858c
Adi Faisal Rahman <adi.faisal@bukalapak.com>
2022-09-14 21:48:08 +0700

    add API config

---
+++ app/src/main/java/com/adifaisalr/tmdbapplication/api/Api.kt
@@ -0,0 +1,28 @@
+package com.adifaisalr.tmdbapplication.api
+
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+object Api {
+    private lateinit var baseConfigurator: ApiBaseConfigurator
+
+    fun setBaseConfigurator(configurator: ApiBaseConfigurator) {
+        baseConfigurator = configurator
+    }
+
+    fun newHttpClientBuilder(): OkHttpClient.Builder {
+        return baseConfigurator.newHttpClientBuilder()
+    }
+
+    const val DEFAULT_BASE_URL = "https://api.github.com/"
+    const val TIMEOUT = 60000
+
+    @Synchronized
+    fun getDefaultClient(): OkHttpClient {
+        return newHttpClientBuilder()
+            .connectTimeout(TIMEOUT.toLong(), TimeUnit.MILLISECONDS)
+            .readTimeout(TIMEOUT.toLong(), TimeUnit.MILLISECONDS)
+            .writeTimeout(TIMEOUT.toLong(), TimeUnit.MILLISECONDS)
+            .build()
+    }
+}
